a14626f470939be71db393dbf289dfb2e51bd8d9
Hope Tambala <hopetambala@outlook.com>
2021-04-14 21:49:47 -0400

    Update index.js

--- components/FormikFields/PaperInputPicker/index.js
+++ components/FormikFields/PaperInputPicker/index.js
@@ -1,11 +1,11 @@
 import { Spinner } from 'native-base';
 import * as React from 'react';
-import { StyleSheet, TouchableWithoutFeedback, Image, Text, View
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableWithoutFeedback
 } from 'react-native';
-<<<<<<< HEAD
-=======
-
->>>>>>> ff2a2e07baacbb67a351accc9e33004fa1f59bf5
 import {
   Button, Headline,
   TextInput,
@@ -15,8 +15,6 @@ import getLocation from '../../../modules/geolocation';
 import I18n from '../../../modules/i18n';
 import { layout, theme } from '../../../modules/theme';
 import PaperButton from '../../Button';
-import UseCameraRoll from '../../Multimedia/CameraRoll';
-import UseCamera from '../../Multimedia/UseCamera';
 import AutoFill from './AutoFill';
 import HouseholdManager from './HouseholdManager';
 import { stylesDefault, stylesPaper, styleX, styleButton, styles } from './index.style';
@@ -59,20 +57,6 @@ const PaperInputPicker = ({
     }
   };
 
-<<<<<<< HEAD
-=======
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      paddingHorizontal: 10,
-    }
-  });
-  const [cameraVisible, setCameraVisible] = React.useState(false);
-  const [pictureUris, setPictureUris] = React.useState({});
-  const [image, setImage] = React.useState(null);
-
->>>>>>> ff2a2e07baacbb67a351accc9e33004fa1f59bf5
   return (
     <>
       {fieldType === 'input' && (
@@ -215,23 +199,7 @@ const PaperInputPicker = ({
                 {/* selected value */}
                 {result.value === values[formikKey] && (
                   <TouchableWithoutFeedback OnPress={() => setFieldValue(formikKey, result.value)}>
-<<<<<<< HEAD
                     <View style={styleButton.selected}>
-=======
-                    <View
-                      style={{
-                        backgroundColor: theme.colors.primary,
-                        borderColor: theme.colors.primary,
-                        borderWidth: 1,
-                        borderRadius: 5,
-                        alignItems: 'center',
-                        color: theme.colors.primary,
-                        paddingHorizontal: 15,
-                        paddingVertical: 5,
-                        margin: 5
-                      }}
-                    >
->>>>>>> ff2a2e07baacbb67a351accc9e33004fa1f59bf5
 
                       <View style={styles.button}>
                         <Text style={{ color: 'white' }}>{customForm ? result.label : I18n.t(result.label)}</Text>
@@ -245,19 +213,7 @@ const PaperInputPicker = ({
                   <TouchableWithoutFeedback
                     onPress={() => setFieldValue(formikKey, result.value)}
                   >
-<<<<<<< HEAD
                     <View style={styleButton.unselected}>
-=======
-                    <View style={{
-                      borderWidth: 1,
-                      borderColor: theme.colors.primary,
-                      borderRadius: 5,
-                      paddingHorizontal: 15,
-                      paddingVertical: 5,
-                      margin: 5
-                    }}
-                    >
->>>>>>> ff2a2e07baacbb67a351accc9e33004fa1f59bf5
                       <Text style={{ color: theme.colors.primary }}>
                         {customForm ? result.label : I18n.t(result.label)}
                       </Text>
@@ -481,61 +437,6 @@ const PaperInputPicker = ({
                 </View>
               )))}
             </View>
-          </View>
-        )
-      }
-      {
-        fieldType === 'photo' && (
-          <View style={stylesDefault.container}>
-            {!cameraVisible && image === null && (
-              <View>
-                <Text style={stylesDefault.labelImage}>{translatedLabel}</Text>
-                <Button onPress={() => setCameraVisible(true)}>Take Photo</Button>
-                <UseCameraRoll
-                  pictureUris={pictureUris}
-                  setPictureUris={setPictureUris}
-                  formikProps={formikProps}
-                  formikKey={formikKey}
-                  image={image}
-                  setImage={setImage}
-                />
-              </View>
-            )}
-            {!cameraVisible && image !== null && (
-              <View>
-                <Text style={stylesDefault.labelImage}>{translatedLabel}</Text>
-                <Image source={{ uri: image }} style={{ width: 'auto', height: 400 }} />
-                <Button onPress={() => {
-                  setCameraVisible(true);
-                }}
-                >
-                  Take Picture
-                </Button>
-                <UseCameraRoll
-                  pictureUris={pictureUris}
-                  setPictureUris={setPictureUris}
-                  formikProps={formikProps}
-                  formikKey={formikKey}
-                  image={image}
-                  setImage={setImage}
-                />
-              </View>
-            )}
-            {cameraVisible && (
-              <View>
-                <Text style={stylesDefault.labelImage}>{label}</Text>
-                <UseCamera
-                  cameraVisible={cameraVisible}
-                  setCameraVisible={setCameraVisible}
-                  pictureUris={pictureUris}
-                  setPictureUris={setPictureUris}
-                  formikProps={formikProps}
-                  formikKey={formikKey}
-                  image={image}
-                  setImage={setImage}
-                />
-              </View>
-            )}
           </View>
         )
       }
