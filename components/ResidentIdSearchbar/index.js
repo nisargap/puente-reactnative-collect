455ba4cf01f8724658ecec788182338219bc16dd
Hope Tambala <hopetambala@outlook.com>
2021-04-22 22:02:13 -0400

    chore: reduce timeout for search

--- components/ResidentIdSearchbar/index.js
+++ components/ResidentIdSearchbar/index.js
@@ -96,7 +96,7 @@ const ResidentIdSearchbar = ({ surveyee, setSurveyee, surveyingOrganization }) =
 
     setSearchTimeout(setTimeout(() => {
       fetchData(online, input);
-    }, 2000));
+    }, 1000));
   };
 
   const onSelectSurveyee = (listItem) => {
