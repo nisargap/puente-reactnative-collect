b5ba3948704307d036927662e5b2e062bb808669
Hope Tambala <hopetambala@outlook.com>
2022-01-30 08:40:57 -0500

    chore: remove parse

--- App.js
+++ App.js
@@ -2,17 +2,15 @@
 import 'react-native-gesture-handler';
 
 import { NavigationContainer } from '@react-navigation/native';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Provider as PaperProvider } from 'react-native-paper';
 
 import MainNavigation from './components/MainNavigation';
 import { UserContextProvider } from './context/auth.context';
 import { OfflineContextProvider } from './context/offline.context';
-import { ParseContextProvider } from './context/parse.context';
-
 import useCachedResources from './modules/cached-resources/useCachedResources';
 import { theme } from './modules/theme';
-// import { initialize } from './services/parse/auth';
+import { initialize } from './services/parse/auth';
 
 // initialize();
 
@@ -22,17 +20,18 @@ export default function App() {
   if (!isLoadingComplete) {
     return null;
   }
+
+  useEffect(() => initialize());
+
   return (
     <NavigationContainer>
-      <ParseContextProvider>
-        <PaperProvider theme={theme}>
-          <UserContextProvider>
-            <OfflineContextProvider>
-              <MainNavigation />
-            </OfflineContextProvider>
-          </UserContextProvider>
-        </PaperProvider>
-      </ParseContextProvider>
+      <PaperProvider theme={theme}>
+        <UserContextProvider>
+          <OfflineContextProvider>
+            <MainNavigation />
+          </OfflineContextProvider>
+        </UserContextProvider>
+      </PaperProvider>
     </NavigationContainer>
 
   );
